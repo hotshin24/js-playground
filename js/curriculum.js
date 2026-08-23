f1d0df38084fd6096720e98a3fa08fb305ce380f
reviewelio24-coder <hotshin24@gmail.com>
2026-08-24 00:48:52 +0900

    fix: 옛 미리보기 URL을 기본 주소로 정리한다

--- js/curriculum.js
+++ js/curriculum.js
@@ -1,2 +1,12 @@
 export const lessonRoot = 'lessons';
 export const storageKey = 'js-playground:v2';
+
+// 미리보기 전환이 끝난 뒤 남아 있는 옛 링크는 같은 정식 과정으로 열되,
+// 주소창에서는 더 이상 존재하지 않는 curriculum 파라미터를 제거한다.
+if (typeof location !== 'undefined') {
+  const url = new URL(location.href);
+  if (url.searchParams.has('curriculum')) {
+    url.searchParams.delete('curriculum');
+    history.replaceState(null, '', url.pathname + url.search + url.hash);
+  }
+}
